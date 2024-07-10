3e50cf5d66fd9a8dec0c35741944aaf64224672c
balibabu <balibabuchauhan1@gmail.com>
2024-07-10 12:52:54 +0545

    added rot state save

--- frontend/src/components/WatchStream.jsx
+++ frontend/src/components/WatchStream.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
 
+const storedValue = localStorage.getItem('rot');
 export default function WatchStream() {
-    const [rotate, setRotate] = useState(0);
+    const [rotate, setRotate] = useState(storedValue ? parseInt(storedValue) : 0);
+
+    function rotationHandler() {
+        localStorage.setItem('rot', rotate + 90);
+        setRotate((prev) => prev + 90);
+    }
 
     return (
         <div className='h-dvh flex justify-center items-center'>
-            <button className='bg-gray-800 p-2 z-20 fixed right-0 top-0' onClick={() => setRotate((prev) => prev + 90)}>rotate</button>
-            <img alt="Live Stream" style={{ transform: `rotate(${rotate}deg)` }}  src="/video" />
+            <button className='bg-gray-800 p-2 z-20 fixed right-0 top-0' onClick={rotationHandler}>rotate</button>
+            <img alt="Live Stream" style={{ transform: `rotate(${rotate}deg)` }} src="/video" />
         </div>
     )
 }
-// src="/video" 
+// src="/video"
 // src='https://media.geeksforgeeks.org/auth-dashboard-uploads/Must-do-coding-questions-with-bg-%281%29.png'
